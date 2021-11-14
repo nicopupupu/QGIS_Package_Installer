9805a967e3e62315dafe7eddfff164acbdbe4c90
nicopupupu <91824117+nicopupupu@users.noreply.github.com>
2021-11-14 15:00:28 +0000

    x86/platform: Fix Geode LX timekeeping in the generic x86 build

--- Attack/output/rplot.r
+++ Attack/output/rplot.r
@@ -5,4 +5,11 @@ setwd(wd)
 print(wd)
 file.names <- dir(wd, pattern=".dat")
 for(i in 1:length(file.names)){
-    w1 <- read.csv(paste(file.names[i], 
+    w1 <- read.csv(paste(file.names[i], sep=""))
+    rev(w1)
+    true <- subset(w1, w1$step4 == 1)
+    false <- subset(w1, w1$step4 == 2)
+    png(paste("plots/", file.names[i], ".png", sep=""))
+    plot(w1$message, w1$duration, col=c("red","blue")[w1$step4], xlab="message", ylab="duration (ns)", pch=".", cex=1)
+#plot(w1$message, w1$duration)
+#abline(lm(w1$duration
