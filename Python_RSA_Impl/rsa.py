9703f1996939f5847229d23a4e87c40f1ad85559
nicopupupu <91824117+nicopupupu@users.noreply.github.com>
2021-12-02 06:21:42 +0000

    line ending

--- Python_RSA_Impl/rsa.py
+++ Python_RSA_Impl/rsa.py
@@ -33,4 +33,14 @@ def ModInverse(a, n):
 	"""
 	(t, new_t, r, new_r) = 0, 1, int(n), int(a)
 	while new_r != 0:
-		quoti
+		quotient = r//new_r
+		(t, new_t) = (new_t, t - quotient * new_t)
+		(r, new_r) = (new_r, r - quotient * new_r)
+	if r > 1:
+		raise ArithmeticError("ERROR: %d is not invertible modulo %d. \n r was: %d, new_r was %d " % (a, n, r, new_r))
+	if t < 0:
+		t = t + n
+	return t
+
+def MongomeryProduct(a, b, nprime, r, n):
+	""" Montgomery product.""
