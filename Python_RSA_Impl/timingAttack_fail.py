e505debe4253c52cac6a1b9af60bb82acf3e4e91
nicopupupu <91824117+nicopupupu@users.noreply.github.com>
2021-12-17 12:57:19 +0000

    Minor style changes

--- Python_RSA_Impl/timingAttack_fail.py
+++ Python_RSA_Impl/timingAttack_fail.py
@@ -6,4 +6,13 @@ from pprint import pprint
 
 def ModInverse(a, n):
 	""" Calculates the modular inverse of a mod n.
-		http://www.wikiwand.com/en/Extended_Euclidean_algorithm#/Modular_
+		http://www.wikiwand.com/en/Extended_Euclidean_algorithm#/Modular_integers
+	"""
+	(t, new_t, r, new_r) = 0, 1, int(n), int(a)
+	while new_r != 0:
+		quotient = r//new_r
+		(t, new_t) = (new_t, t - quotient * new_t)
+		(r, new_r) = (new_r, r - quotient * new_r)
+	if r > 1:
+		raise ArithmeticError("ERROR: %d is not invertible modulo %d. \n r was: %d, new_r was %d " % (a, n, r, new_r))
+	if t
