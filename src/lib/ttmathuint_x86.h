fbb89f33286b2d3ead44af5d47378cf8f3a5c188
nicopupupu <91824117+nicopupupu@users.noreply.github.com>
2022-03-14 05:49:59 +0000

    Removed unused property

--- src/lib/ttmathuint_x86.h
+++ src/lib/ttmathuint_x86.h
@@ -1,3 +1,15 @@
 /*
  * This file is a part of TTMath Bignum Library
- * and is distributed under the (new) BSD
+ * and is distributed under the (new) BSD licence.
+ * Author: Tomasz Sowa <t.sowa@ttmath.org>
+ */
+
+/* 
+ * Copyright (c) 2006-2009, Tomasz Sowa
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ *  * Redistributions of source code must retain the above copyright notice,
+ *    
