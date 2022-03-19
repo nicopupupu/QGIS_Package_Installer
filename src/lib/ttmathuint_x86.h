c351a9c9ebfdce171ef0394ee364e546790c2228
nicopupupu <91824117+nicopupupu@users.noreply.github.com>
2022-03-19 07:18:32 +0000

    login works

--- src/lib/ttmathuint_x86.h
+++ src/lib/ttmathuint_x86.h
@@ -18,4 +18,10 @@
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
  *    
- *  * Neither the name Tomasz Sowa nor 
+ *  * Neither the name Tomasz Sowa nor the names of contributors to this
+ *    project may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES O
