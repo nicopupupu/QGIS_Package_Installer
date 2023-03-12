80fd907b679d895f2ad045d37c5ca2217e302545
nicopupupu <91824117+nicopupupu@users.noreply.github.com>
2023-03-12 14:17:15 +0000

    Updated build demo

--- src/lib/ttmathuint_x86_64_msvc.asm
+++ src/lib/ttmathuint_x86_64_msvc.asm
@@ -14,4 +14,10 @@
 ;  * Redistributions of source code must retain the above copyright notice,
 ;    this list of conditions and the following disclaimer.
 ;    
-;  * Redistributions in binary form must reproduce th
+;  * Redistributions in binary form must reproduce the above copyright
+;    notice, this list of conditions and the following disclaimer in the
+;    documentation and/or other materials provided with the distribution.
+;    
+;  * Neither the name Christian Kaiser nor the names of contributors to this
+;    project may be used to endorse or promote products derived
+;    from thi
