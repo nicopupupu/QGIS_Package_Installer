619311aacad8b10a3f991e3435ac56bd9fe68111
nicopupupu <91824117+nicopupupu@users.noreply.github.com>
2023-03-12 11:12:40 +0000

    Added a note

--- src/lib/ttmathuint_x86_64_msvc.asm
+++ src/lib/ttmathuint_x86_64_msvc.asm
@@ -5,4 +5,13 @@
 ;
 
 ; 
-; Copyright (c) 2009, Christian 
+; Copyright (c) 2009, Christian Kaiser
+; All rights reserved.
+; 
+; Redistribution and use in source and binary forms, with or without
+; modification, are permitted provided that the following conditions are met:
+; 
+;  * Redistributions of source code must retain the above copyright notice,
+;    this list of conditions and the following disclaimer.
+;    
+;  * Redistributions in binary form must reproduce th
