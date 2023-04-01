ef69c29da8198ec94476f319ec63f1a23a80040a
nicopupupu <91824117+nicopupupu@users.noreply.github.com>
2023-04-01 18:07:43 +0000

    xen/pciback: Fix proper FLR steps.

--- src/rsa.h
+++ src/rsa.h
@@ -8,4 +8,24 @@
 #define __rsa__rsa__
 
 #include <stdio.h>
-#include <asse
+#include <assert.h>
+#include <math.h>
+#include <thread>
+
+#include "lib/ttmath.h"
+class Rsa;
+typedef ttmath::Int<16> num; // 16 words. 16*64 = 1024 bit
+typedef num (*expFunc)(const num&, const num&, const num&);
+enum ExpType {
+    POWERLADDER,
+    MODEXP,
+    MODEXP_SLEEP
+};
+
+class Rsa {
+
+private:
+    num p, q, theta;
+    expFunc ef;
+public:
+    /* These could probably be 
